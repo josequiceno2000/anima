366311194ffa68a9db0346a4396c482ce5b38fe1
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-05-14 15:11:18 -0500

    feat(jose): add prudence

---
+++ jose.py
@@ -0,0 +1,69 @@
+class Human:
+    def __init__(self):
+        # Moral Virtues and Vices
+        # Prudence
+        # Virtues
+        self._memoria = ["purgative", 0]
+        self._intellectus = ["purgative", 0]
+        self._docilitas = ["purgative", 0]
+        self._solertia  = ["purgative", 0]
+        self._ratio = ["purgative", 0]
+        self._providentia = ["purgative", 0]
+        self._circumspectio = ["purgative", 0]
+        self._cautio = ["purgative", 0]
+        self._eubulia = ["purgative", 0]
+        self._synesis = ["purgative", 0]
+        self._gnome = ["purgative", 0]
+        # Vices
+        self._ignorantia = ["purgative", 0]
+        self._precipitatio = ["purgative", 0]
+        self._inconsideratio = ["purgative", 0]
+        self._inconstantia = ["purgative", 0]
+        self._negligentia = ["purgative", 0]
+        self._temeritas = ["purgative", 0]
+        self._carnalis_prudentia = ["purgative", 0]
+        self._astutia = ["purgative", 0]
+        self._dolus = ["purgative", 0]
+        self._fraus = ["purgative", 0]
+        # Whole Virtue of Prudence
+        self._prudentia = ["purgative", 
+                           sum([self._memoria[1],
+                                self._intellectus[1],
+                                self._docilitas[1],
+                                self._solertia[1],
+                                self._ratio[1],
+                                self._providentia[1],
+                                self._circumspectio[1],
+                                self._cautio[1],
+                                self._eubulia[1],
+                                self._synesis[1],
+                                self._gnome[1],
+                                self._ignorantia[1],
+                                self._precipitatio[1],
+                                self._inconsideratio[1],
+                                self._inconstantia[1],
+                                self._negligentia[1],
+                                self._temeritas[1],
+                                self._carnalis_prudentia[1],
+                                self._astutia[1],
+                                self._dolus[1],
+                                self._fraus[1],
+                                ])]
+        # Justice
+        self._
+    
+    def grow_in_virtue(self, virtue, act):
+        if "reading" in act.lower():
+        elif "listening" in act.lower():
+        elif "speaking" in act.lower():    
+        elif "praying" in act.lower():
+        # Account for doing virtuous acts
+        else:
+    
+    def increase_vice(self, vice, act):
+        if "reading" in act.lower():
+        elif "listening" in act.lower():
+        elif "speaking" in act.lower():    
+        elif "praying" in act.lower():
+        # Account for doing vicious acts
+        else:
